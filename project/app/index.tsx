9340a5e7910edfcadda41b0d13dd7824c3748187
Nareshm03 <nareshmurthy080@gmail.com>
2025-05-08 22:20:41 +0530

    initial

--- project/app/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Redirect } from 'expo-router';
-
-// Redirect to the tabs layout as the main entry point
-export default function Index() {
-  return <Redirect href="/(auth)/login" />;
-}
